f798274c3ad18a4e09eef452114f381b8ab4aa28
Yusuf Greer <ycgrhux9yw@users.noreply.github.com>
2026-04-15 03:58:25 +0800

    feat: add main entry point

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+// Package main is the entry point for the powertracker application.
+// powertracker monitors and tracks power consumption data from compatible devices.
 package main
 
 import "github.com/poolski/powertracker/cmd"
