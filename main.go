f25bef006543771c1a2427c638e1997770ab9343
Yusuf Greer <ycgrhux9yw@users.noreply.github.com>
2026-04-15 22:16:35 +0800

    docs: add personal fork note to package comment

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 // Package main is the entry point for the powertracker application.
 // powertracker monitors and tracks power consumption data from compatible devices.
+//
+// This is a personal fork of github.com/poolski/powertracker used for
+// experimenting with home energy monitoring.
 package main
 
 import "github.com/poolski/powertracker/cmd"
