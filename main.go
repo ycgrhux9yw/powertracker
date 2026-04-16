6a69adfafff7c076b5b7f7b56f7e3733eab407f0
Yusuf Greer <ycgrhux9yw@users.noreply.github.com>
2026-04-16 08:36:54 +0800

    docs: add personal fork notes about home setup to package comment

--- main.go
+++ main.go
@@ -3,6 +3,10 @@
 //
 // This is a personal fork of github.com/poolski/powertracker used for
 // experimenting with home energy monitoring.
+//
+// Fork notes:
+//   - Tracking energy usage for my home setup (3-bed house, UK)
+//   - Main devices: EV charger, heat pump, solar inverter
 package main
 
 import "github.com/poolski/powertracker/cmd"
